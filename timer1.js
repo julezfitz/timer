21dae8a2c10732e55ca0352f58c407e864d92636
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-08-11 15:55:54 +0000

    created timer function

---
+++ timer1.js
@@ -0,0 +1,15 @@
+const { map } = require("../../w1/lotide");
+
+let args = (process.argv.slice(2)).map(i => Number(i));
+
+const timer = function(args) {
+  for (let i = 0; i < args.length; i++) {
+    if (typeof args[i] === 'number' && (args[i] > 0)) {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+      }, args[i] * 1000);
+    }
+  }
+};
+
+timer(args);
